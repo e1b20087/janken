c9f31c15a3d41efb14f02f6ffcf6f113311da493
e1b20087 <e1b20087@oit.ac.jp>
2022-10-08 21:00:00 +0900

    コントローラーを変更

--- src/main/java/oit/is/z0966/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0966/kaizi/janken/controller/JankenController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Controller
 public class JankenController {
 
   @GetMapping("/janken")
